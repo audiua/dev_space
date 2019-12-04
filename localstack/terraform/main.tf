77cb40e5986a98973625b79092890a0b119087b2
Костя Тарнашинский <kostia.tarnashynskyi@gmail.com>
2019-12-04 15:40:20 +0200

    terraform + localstack

--- localstack/terraform/main.tf
+++ localstack/terraform/main.tf
@@ -1,6 +1,19 @@
 provider "aws" {
+  skip_credentials_validation = true
+  skip_metadata_api_check     = true
+  s3_force_path_style         = true
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
+  region = "us-east-1"
+
   endpoints {
-    dynamodb = "http://localhost:4569"
-    s3       = "http://localhost:4572"
+    ec2 = "http://localhost:4597"
+//    dynamodb = "http://localhost:4569"
+//    s3       = "http://localhost:4572"
   }
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-2757f631"
+  instance_type = "t2.micro"
 }
